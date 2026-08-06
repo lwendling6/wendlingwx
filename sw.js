a1fc6ed5db67487a1c4b4e934120b6f28ab113fb
lwendling6 <loganwendling6@gmail.com>
2026-08-06 11:01:20 -0800

    Update cache name for service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this cache name whenever index.html, codec.js, or manifest.json changes.
-const CACHE = 'wendlingwx-2026-08-05e';
+const CACHE = 'wendlingwx-2026-08-06a';
 const FILES = ['./', './index.html', './codec.js', './manifest.json'];
 
 self.addEventListener('install', e => {
